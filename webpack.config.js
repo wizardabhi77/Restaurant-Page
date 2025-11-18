6c514b4fb615480af5cc3addc9c5810ef3a26720
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-11-18 10:30:31 +0530

    Create index.js and conifigure webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename : "main.js",
+        path: path.resolve(__dirname,"dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: "./src/template.html",
+            }
+        ),
+    ],
+};
